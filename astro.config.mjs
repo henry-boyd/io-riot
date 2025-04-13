d3f27f7941f7a1c19b77bfc2e4427c73eaf76f72
Henry Boyd <48170095+henry-boyd@users.noreply.github.com>
2025-04-13 09:03:18 -0600

    added ethics policy page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://techandconsequence.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [sitemap(), pagefind()],
 });
