940f3b03f81099ee0bad48b86f2e0e5d42b0e0fb
Henry Boyd <48170095+henry-boyd@users.noreply.github.com>
2026-01-15 02:05:02 -0700

    vercel updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import pagefind from "astro-pagefind";
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ioriot.com',
-	integrations: [sitemap(), pagefind()],
+  site: 'https://ioriot.com',
+  integrations: [sitemap(), pagefind()],
+  adapter: vercel(),
 });
