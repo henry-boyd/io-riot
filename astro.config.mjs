c073c3eefb9837629dfdf5362b2337a29dff5653
Henry Boyd <48170095+henry-boyd@users.noreply.github.com>
2026-03-21 16:23:20 -0600

    Cloudflare Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import pagefind from "astro-pagefind";
 
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ioriot.com',
   integrations: [sitemap(), pagefind()],
-  adapter: vercel(),
 });
